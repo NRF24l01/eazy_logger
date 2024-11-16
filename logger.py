7d953eec985fa88fc6cbd4cc22138fc015e11e45
$sudo heck beluga <96012285+NRF24l01@users.noreply.github.com>
2024-11-16 21:30:34 +0300

    Update logger to input like in print

--- logger.py
+++ logger.py
@@ -1,7 +1,7 @@
 import logging
 
 class Logger:
-    def __init__(self, filename):
+    def __init__(self, filename=None):
         self.filename = filename
         self.logger = logging.getLogger("my_logger")
         self.logger.setLevel(logging.DEBUG)
@@ -11,22 +11,26 @@ class Logger:
         self.console_handler.setFormatter(self.console_formatter)
         self.logger.addHandler(self.console_handler)
 
-        self.file_handler = logging.FileHandler(self.filename, encoding='utf-8')
-        self.file_formatter = logging.Formatter(u'%(asctime)s - %(levelname)s - %(message)s')
-        self.file_handler.setFormatter(self.file_formatter)
-        self.logger.addHandler(self.file_handler)
+        if filename:
+            self.file_handler = logging.FileHandler(self.filename, encoding='utf-8')
+            self.file_formatter = logging.Formatter(u'%(asctime)s - %(levelname)s - %(message)s')
+            self.file_handler.setFormatter(self.file_formatter)
+            self.logger.addHandler(self.file_handler)
 
-    def info(self, message):
+    def info(self, *args):
+        message = " ".join(map(str, args))
         self.logger.info(message)
 
-    def warning(self, message):
+    def warning(self, *args):
+        message = " ".join(map(str, args))
         self.logger.warning(message)
 
-    def error(self, message):
+    def error(self, *args):
+        message = " ".join(map(str, args))
         self.logger.error(message)
 
-if __name__ == "main":
-    logger = Logger("log.txt")
-    logger.info("Это информационное сообщение")
-    logger.warning("Это предупреждение")
-    logger.error("Это сообщение об ошибке")
+if __name__ == "__main__":
+    logger = Logger()
+    logger.info("Это", "информационное", "сообщение", 123)
+    logger.warning("Это", "предупреждение", {"ключ": "значение"})
+    logger.error("Это", "сообщение", "об ошибке", [1, 2, 3])
